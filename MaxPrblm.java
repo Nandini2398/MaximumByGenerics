1e620492ec4b2d020ae25962cbe73287b83d3e1a
Nandini2398 <yarranandhini1416@gmail.com>
2021-12-26 10:48:34 +0530

    UPDATED:max three strings

--- MaxPrblm.java
+++ MaxPrblm.java
@@ -2,7 +2,6 @@ package com.bridgelabz.maxprblm;
 
 public class MaxPrblm {
 public static void main(String[] args) {
-		
 		Operation operation = new Operation();
 		operation.getInputForInteger();
 		operation.testMaximumInteger(operation.firstNumber,operation.secondNumber,operation.thirdNumber);
@@ -10,6 +9,9 @@ public static void main(String[] args) {
 		operation.getInputForFloat();
 		operation.testMaximumFloat(operation.number1,operation.number2,operation.number3);
 		System.out.println(operation.max2+" is maximum amongst float numbers");
+		operation.getInputForString();
+		operation.testMaximumString(operation.string1,operation.string2,operation.string3);
+		System.out.println(operation.max3+" is maximum amongst strings");
 	}
 }
 
